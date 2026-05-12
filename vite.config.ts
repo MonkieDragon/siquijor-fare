d5a78bbe64f7eaa22ee1801b5e9f6ceb01ed1b20
MonkieDragon <clivejeffries@hotmail.com>
2026-05-12 17:17:43 +0800

    changed setting to make it build on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 
+const repoSegment = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base = repoSegment ? `/${repoSegment}/` : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
+
   plugins: [react()],
 
   test: {
